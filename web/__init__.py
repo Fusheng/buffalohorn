883b8d2fe1f57c141f9a3026b32923b5a97006b4
baifusheng <baifusheng@gmail.com>
2019-01-12 16:39:10 +0800

    modified some issues

--- web/__init__.py
+++ web/__init__.py
@@ -10,12 +10,13 @@ from google.appengine.api.mail import send_mail
 from common import commonInfo
 
 FEEDBACK_SENT_TO_EMAIL='baifusheng@gmail.com'
-ADMIN_EMAIL='baifusheng@gmail.com'
+ADMIN_EMAIL = 'baifusheng@gmail.com'
+
 
 def feedback(request):
     email = request.POST.get('email')
     content = request.POST.get('content')
-    if not content:
+    if not content or not email or not email.index('@'):
         t = loader.get_template('web/feedback.html')
         c = Context({})
         response = HttpResponse(t.render(c))
--- web/g.py
+++ web/g.py
@@ -139,7 +139,7 @@ class GoogleAPI:
 
         for p in range(0, pages):
             start = p * 10
-            url = '%s/search?hl=%s&num=%d&start=%s&q=%s' % (base_url, lang, 10, start, query)
+            url = '%s?hl=%s&num=%d&start=%s&q=%s' % (base_url, lang, 10, start, query)
             try:
                 request = urllib2.Request(url)
                 request.add_header('User-agent', user_agent)
@@ -155,6 +155,7 @@ class GoogleAPI:
                 print 'url error:', e
             except Exception, e:
                 print 'error:', e
+                return e
         return search_results
 
 
@@ -170,7 +171,8 @@ def crawler(keyword):
 
 
 pages = 1
-base_url = "https://www.google.com"
+base_url = "https://www.google.com/search"
+# base_url = "https://www.baidu.com/s"
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
 
 
